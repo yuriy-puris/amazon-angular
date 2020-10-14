ec25a74c41746483874944752546aae3465a1e07
yuriy <donovanyuriy@gmail.com>
2020-10-14 21:55:48 +0300

    product

--- routes/main.js
+++ routes/main.js
@@ -4,19 +4,6 @@ const Category = require('../models/category');
 const Product = require('../models/product');
 
 
-router.get('/test', (req, res, next) => {
-	const number1 = cb => {
-		const firstName = 'test1';
-		cb(null, firstName);
-	};
-	const number2 = (firstName, cb) => {
-		const lastName = 'test2';
-		console.log(`${firstName}${lastName}`)
-	};
-
-	async.waterfall([number1, number2]);
-});
-
 router.route('/categories')
 	.get((req, res, next) => {
 		Category.find({}, (err, categories) => {
@@ -38,22 +25,102 @@ router.route('/categories')
 	})
 
 router.get('/categories/:id', (req, res, next) => {
-	console.log(req.params)
 	const perPage = 10;
-	Product.find({ category: req.params.id })
-		.populate('category')
-		.exec((err, products) => {
-			Product.count({ category: req.params.id }, (err, totalProducts) => {
-				res.json({
-					success: true,
-					message: 'category',
-					products: 'products',
-					categoryName: products[0].category.name,
-					totalProducts: totalProducts,
-					pages: Math.ceil(totalProducts / perPage)
-				})
-			})
-		})
+    const page = req.query.page;
+    async.parallel([
+      function(callback) {
+        Product.count({ category: req.params.id }, (err, count) => {
+          var totalProducts = count;
+          callback(err, totalProducts);
+        });
+      },
+      function(callback) {
+        Product.find({ category: req.params.id })
+          .skip(perPage * page)
+          .limit(perPage)
+          .populate('category')
+          .populate('owner')
+          .populate('reviews')
+          .exec((err, products) => {
+            if(err) return next(err);
+            callback(err, products);
+          });
+      },
+      function(callback) {
+        Category.findOne({ _id: req.params.id }, (err, category) => {
+         callback(err, category)
+        });
+      }
+    ], function(err, results) {
+      var totalProducts = results[0];
+      var products = results[1];
+      var category = results[2];
+      res.json({
+        success: true,
+        message: 'category',
+        products: products,
+        categoryName: category.name,
+        totalProducts: totalProducts,
+        pages: Math.ceil(totalProducts / perPage)
+      });
+    });
 });
+
+router.get('/products', (req, res, next) => {
+	const perPage = 10;
+	const page = req.query.page;
+	async.parallel([
+		function(callback) {
+		  Product.count({}, (err, count) => {
+			var totalProducts = count;
+			callback(err, totalProducts);
+		  });
+		},
+		function(callback) {
+		  Product.find({})
+			.skip(perPage * page)
+			.limit(perPage)
+			.populate('category')
+			.populate('owner')
+			.exec((err, products) => {
+			  if(err) return next(err);
+			  callback(err, products);
+			});
+		}
+	  ], function(err, results) {
+		var totalProducts = results[0];
+		var products = results[1];
+	   
+		res.json({
+		  success: true,
+		  message: 'category',
+		  products: products,
+		  totalProducts: totalProducts,
+		  pages: Math.ceil(totalProducts / perPage)
+		});
+	  });
+});
+
+
+router.get('/product/:id', (req, res, next) => {
+	Product.findById({ _id: req.params.id })
+		.populate('category')
+		.populate('owner')
+		.exec((err, product) => {
+			if ( err ) {
+				res.json({
+					success: false,
+					message: 'Product is not found'
+				})
+			} else {
+				if ( product ) {
+					res.json({
+						success: true,
+						product: product
+					})
+				}
+			}
+		})
+})
 
 module.exports = router;
